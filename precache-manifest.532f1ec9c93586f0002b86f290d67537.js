814a53cebf0ebdaf64f7155ee2ab06dc156e8987
xakim <xakim@pop-os.localdomain>
2020-10-01 15:33:20 +0600

    Updates

---
+++ precache-manifest.532f1ec9c93586f0002b86f290d67537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b9bfd8bd6eebb3efa016321a4c4c43",
+    "url": "/PoliticalCompass/index.html"
+  },
+  {
+    "revision": "94c14441c9ce68e96420",
+    "url": "/PoliticalCompass/static/css/main.eefea701.chunk.css"
+  },
+  {
+    "revision": "2a28c383ce4a519078eb",
+    "url": "/PoliticalCompass/static/js/2.7263b0bd.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/PoliticalCompass/static/js/2.7263b0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c14441c9ce68e96420",
+    "url": "/PoliticalCompass/static/js/main.3d0747ee.chunk.js"
+  },
+  {
+    "revision": "6c301e307cbf8e0ac3e4",
+    "url": "/PoliticalCompass/static/js/runtime-main.9b5d5903.js"
+  }
+]);
